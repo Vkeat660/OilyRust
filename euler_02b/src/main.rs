8d8a6ef6b615e29070d5e12ba111aa0289da820e
Vivian Keating <vkeat660@gmail.com>
2016-03-13 20:48:41 -0400

    Relatively happy with the alternative solution

--- euler_02b/src/main.rs
+++ euler_02b/src/main.rs
@@ -1,36 +1,32 @@
 struct Fibonacci {
-    curr: u32,
-    next: u32,
+    curr: u64,
+    next: u64,
 }
 
-// Implement 'Iterator' for 'Fibonacci'
-impl Iterator for Fibonacci {
-    type Item = u32;
-    // The 'Iterator' trait only requires the 'next' method to be defined. The
-    // return type is 'Option<T>', 'None' is returned when the 'Iterator' is
-    // over, otherwise the next value is returned wrapped in 'Some'
-    fn next(&mut self) -> Option<u32> {
-        let new_next = self.curr + self.next;
+impl Iterator for Fibonacci{
+    type Item = u64;
 
-        self.curr = self.next;
-        self.next = new_next;
+    fn next(&mut self) -> Option<u64> {
+      let new_next = self.curr + self.next;
 
-        // 'Some' is always returned, this is an infinite value generator
-        Some(self.curr)
+      self.curr = self.next;
+      self.next = new_next;
+
+      Some(self.curr)
     }
 }
 
-// Returns a fibonacci sequence generator
 fn fibonacci() -> Fibonacci {
-    Fibonacci { curr: 1, next: 1 }
+    Fibonacci { curr: 0, next: 1 }
 }
 
 fn main() {
 
-  let sum : u32 = fibonacci()
+  let sum : u64 = fibonacci()
     .take_while(|&i| i<4_000_000)
     .filter(|&i| i%2 == 0)
     .fold(0, |a,b| a+ b);
 
   println!("The sum is {}", sum);
+
 }
