4a1a959cb8fa81443911bbb61236d186cf8e5e11
Vivian Keating <vkeat660@gmail.com>
2016-03-13 20:07:11 -0400

    Solved Euluer 02 a bad way

--- euler_02/src/main.rs
+++ euler_02/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+
+    let mut sum = 0;
+    let mut n_minus_one = 1;
+    let mut n_minus_two = 0;
+    loop {
+      let tmp = n_minus_one;
+      n_minus_one += n_minus_two;
+      n_minus_two = tmp;
+
+      if n_minus_one % 2 == 0 {
+        sum += n_minus_one;
+      }
+
+      if n_minus_one > 4_000_000 {
+
+        println!("sum {}", sum);
+        break;
+      }
+
+    }
+
 }
